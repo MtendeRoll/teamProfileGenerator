6c5e83949a88eccd47b09e8d369dcad4616b1db8
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-06-26 12:42:32 -0400

    added name constructor to employee file

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,5 +1,5 @@
 const Employee = require("../lib/Employee");
 
-test(" adds 1 + 2 to equal 3", () => {
-  expect(Employee(1, 2)).toBe(3);
-});
+//tests psuedo code
+//
+test("creates a name object", () => {});
