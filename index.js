87147698104858b020d08b5237bb228ae3cafe93
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-06-26 13:16:40 -0400

    created questions for each addition team role

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 //Packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 //Add validation when puting input
 
 // An array of questions for user input
-const questions = [
+const managerQuestions = [
   {
     type: "input",
     name: "name",
@@ -64,6 +66,116 @@ const questions = [
     name: "addTeamMember",
     message: "Which type of team member would you like to add?",
     choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+  },
+];
+
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the engineer's name?",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter the engineer's name!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the engineer's id?",
+    validate: (idInput) => {
+      if (idInput) {
+        return true;
+      } else {
+        console.log("Please enter the team manager's id number!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the engineer's email?",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Please enter the engineer's email address!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the engineer's GitHub username?",
+    validate: (githubInput) => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log("Please enter the engineer's GitHub username!");
+        return false;
+      }
+    },
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the intern's name?",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log("Please enter the intern's name!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the intern's id?",
+    validate: (idInput) => {
+      if (idInput) {
+        return true;
+      } else {
+        console.log("Please enter the intern's id number!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the intern's email?",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log("Please enter the intern's email address!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What is the intern's school?",
+    validate: (schoolInput) => {
+      if (schoolInput) {
+        return true;
+      } else {
+        console.log("Please enter the intern's school!");
+        return false;
+      }
+    },
   },
 ];
 
